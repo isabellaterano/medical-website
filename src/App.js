3fc846ed5af36a91ff75fe2349f61f37d22c58f6
isabellaterano <teranoiisabella@gmail.com>
2023-11-25 11:59:07 -0500

    update components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
+import BookingConsultation from './Components/BookingConsultation';
+import Notification from './Components/Notification/Notification';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar/>
+          <Notification/>
+          <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Sign_Up" element={<Sign_Up/>}/>
+              <Route path="/Login" element={<Login/>}/>
+              <Route path="/Reviews" element={<ReviewForm/>}/>
+              <Route path="/BookingConsultation" element={<BookingConsultation/>} />
+              <Route path="/ReportsLayout" element={<ReportsLayout/>} />
+          </Routes>
+            
+        </BrowserRouter>
+       
     </div>
   );
 }
